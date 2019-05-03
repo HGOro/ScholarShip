85c3ea203bba6e0ef5aa90b3111912700965a019
HG_ORO <h.g.orozco@gmail.com>
2019-05-03 01:23:18 -0700

    reverts routes/api/index.js to michelle/master code

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,19 +1,6 @@
-
 const router = require("express").Router();
-
+const abcRoutes = require("./abc");
 const researchRoutes = require("./research")
-console.log("Checking api/index.js")
-
-
-//Hernan added=================================
-const UserRoutes = require("./user");
-const AccountRoutes = require("./account");
-const QuizRoutes = require("./quiz");
-
-router.use("/user", UserRoutes);
-router.use("/account", AccountRoutes);
-router.use("/quiz", QuizRoutes);
-//end Hernan added
 
 //research routes
 router.use("/research", researchRoutes);
