ca8665074dd1aa16c8e2dda471b671b12e01d65d
HG_ORO <h.g.orozco@gmail.com>
2019-05-03 00:59:25 -0700

    changes client/src/App.js back to pre-userauth code

--- client/src/App.js
+++ client/src/App.js
@@ -1,118 +1,27 @@
-// import React from 'react';
-// import './App.css';
-
-// import { BrowserRouter as Router, Route } from "react-router-dom";
-// import Navbar from "./components/Navbar";
-// import Home from "./pages/Home";
-// import Elementary from "./pages/Elementary";
-// import Research from "./pages/Research";
-// import User from "./pages/User";
-
-// function App() {
-//   return (
-//     <Router>
-//       <div>
-//         <Navbar />
-//         <Route exact path="/" component={Home} />
-//         <Route exact path="/elementary" component={Elementary} />
-//         <Route exact path="/research" component={Research} />
-//         <Route path="/user" component={User} />
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-import React, { Component } from 'react';
-import API from "./utils/API";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import UserRoutes from './routes/UserRoutes'
-import GuestRoutes from './routes/GuestRoutes'
-import Header from './components/Header'
-import Footer from './components/Footer'
-//import Heading from './components/Header'
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  
-  state = {
-    isLoggedIn: false
-  }
-  
-  componentWillMount(){
-    this.checkAuth();
-    //URL fixed remove anything from url, we want just "/"
-    //let location = window.location.href.split("3000")
-    //location = location[1]
-    //if(this.state.isLoggedIn === false && location !== "/"){
-    //  window.location.href = "/"
-    //}
-  }
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Elementary from "./pages/Elementary";
+import Research from "./pages/Research";
+import MyProject from "./pages/MyProject";
+import User from "./pages/User";
 
-  checkAuth(){
-    API.checkAuth()
-    .then(data => {return data.json()})
-    .then(response => {
-      this.setState({
-        isLoggedIn: response
-      })
-    })
-    .catch(err => console.log("err", err));
-  }
-
-  guestRouting = () => {
-    return(
-      <Router>
-        <div className="content">
-          <Header isLoggedIn={this.state.isLoggedIn}/>
-          <div className="main">
-            <Switch>
-              <Route path="*" component={GuestRoutes} />
-            </Switch>
-          </div>
-          <Footer/>
-        </div>
-      </Router>
-    )
-  }
-
-  userRouting = () => {
-    return(
-      <Router>
-        <div className="content">
-          <Header isLoggedIn={this.state.isLoggedIn} />
-          <div className="main"> 
-            <Switch>
-              <Route exact path="/" component={UserRoutes} />
-              <Route exact path="/dashboard" component={UserRoutes} />
-              <Route path="/dashboard/*" component={UserRoutes} />
-              <Route exact path="/subjects" component={GuestRoutes} />
-              <Route exact path="/math/quiz" component={GuestRoutes} />
-              <Route path="/math/*" component={GuestRoutes} />
-              <Route path="/math" component={GuestRoutes} />
-              <Route path="*" component={GuestRoutes} />
-            </Switch>
-          </div>
-          <Footer/>
-        </div>
-      </Router>
-    )
-  }
-
-  render() {
-    switch(this.state.isLoggedIn){
-      case false:
-       return this.guestRouting()
-      case true:
-       return this.userRouting()
-      default:
-       return this.guestRouting()
-    }
-   }
-
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/elementary" component={Elementary} />
+        <Route exact path="/research" component={Research} />
+        <Route exact path="/myproject" component={MyProject} />
+        <Route path="/user" component={User} />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
-
-
