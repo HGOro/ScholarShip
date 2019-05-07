82af27b3ec3209abe0e89d5d6abf511758ab15b7
HG_ORO <h.g.orozco@gmail.com>
2019-05-06 17:21:21 -0700

    ran npm build

---
+++ client/build/precache-manifest.603c0f5501019a87ec74aa885b2846ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13cca525dd84590d0810abba6846bf84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "788362301f5c90cbd4ee",
+    "url": "/static/css/main.01af673d.chunk.css"
+  },
+  {
+    "revision": "4596afac1cd27b97baaf",
+    "url": "/static/js/2.a77227ab.chunk.js"
+  },
+  {
+    "revision": "788362301f5c90cbd4ee",
+    "url": "/static/js/main.ad5b718f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6e09f4102b0d64adc38d21bd9c89a9a7",
+    "url": "/static/media/hero.6e09f410.jpg"
+  },
+  {
+    "revision": "0b7cabd68aec76c518448ef6d409fa40",
+    "url": "/static/media/logo.0b7cabd6.png"
+  }
+]);
